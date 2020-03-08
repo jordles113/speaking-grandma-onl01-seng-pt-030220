b7ce228eb66691f04abeb56d65b890a9c33b90ea
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-08 01:54:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,11 +17,13 @@
 ## OUTPUTS --> String 
 
 def speak_to_grandma(phrase)
+  answer = "HUH?! SPEAK UP, SONNY!"
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase == "HUH?! SPEAK UP, SONNY!"
+  else 
+    answer
   end 
   
 end 
